c51313ec37f34d0f1dcb9d3fe251d7143b05ec3f
Sebastián <se.salgado@duocuc.cl>
2024-05-03 07:27:27 -0400

    Se cambia font

--- staticfiles/core/js/toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const navToggle = document.querySelector(".nav__toggle");
-    const navMenu = document.querySelector(".nav-menu");
-
-    navToggle.addEventListener("click", () => {
-        navMenu.classList.toggle("nav-menu_visible");
-    });
-});
