19173e2f8544b8add386d27edc74c2276287df27
Sebastián <se.salgado@duocuc.cl>
2024-06-17 12:34:02 -0400

    Se agregar a inicio galeria de imagenes

--- core/static/core/js/sub-menu.js
+++ core/static/core/js/sub-menu.js
@@ -1,7 +1,7 @@
 // Agregar la clase 'sub__menu_active' al submenú si la página actual está dentro del submenú activo
-window.addEventListener('DOMContentLoaded', function() {
-    var currentPagePath = "{{ request.path }}";
-    if (currentPagePath === "/fotografias/" || currentPagePath === "/videos/") {
-        document.querySelector('.sub__menu').classList.add('sub__menu_active');
-    }
-});
+// window.addEventListener('DOMContentLoaded', function() {
+//     var currentPagePath = "{{ request.path }}";
+//     if (currentPagePath === "/fotografias/" || currentPagePath === "/videos/") {
+//         document.querySelector('.sub__menu').classList.add('sub__menu_active');
+//     }
+// });
